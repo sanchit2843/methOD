d06b6b6b5829ea7dcd4ca2ac16c838cdbaf572c0
sanchit2843 <sanchittanwar75@gmail.com>
2024-04-14 20:44:31 +0530

    added centernet based on damoyolons

--- monodle/lib/helpers/model_helper.py
+++ monodle/lib/helpers/model_helper.py
@@ -1,9 +1,11 @@
 from lib.models.centernet3d import CenterNet3D
-
+from lib.models.centernet_damo import CenterNet3DDamo
 
 def build_model(cfg):
     if cfg['type'] == 'centernet3d':
         return CenterNet3D(backbone=cfg['backbone'], neck=cfg['neck'], num_class=cfg['num_class'])
+    elif cfg["type"] == "damoyolo":
+        return CenterNet3DDamo(backbone=cfg['backbone'], neck=cfg['neck'], num_class=cfg['num_class'])
     else:
         raise NotImplementedError("%s model is not supported" % cfg['type'])
 
