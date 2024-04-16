c9ceb78efaf0a3866d9360dd6c6078e4bed209cd
sanchit2843 <sanchittanwar75@gmail.com>
2024-04-16 12:44:45 +0530

    ranger optimizer added

--- monodle/lib/helpers/optimizer_helper.py
+++ monodle/lib/helpers/optimizer_helper.py
@@ -2,30 +2,36 @@ import math
 import torch
 import torch.optim as optim
 from torch.optim.optimizer import Optimizer
+from ranger21 import Ranger21
+
 
 def build_optimizer(cfg_optimizer, model):
     weights, biases = [], []
     for name, param in model.named_parameters():
-        if 'bias' in name:
+        if "bias" in name:
             biases += [param]
         else:
             weights += [param]
 
-    parameters = [{'params': biases, 'weight_decay': 0},
-                  {'params': weights, 'weight_decay': cfg_optimizer['weight_decay']}]
+    parameters = [
+        {"params": biases, "weight_decay": 0},
+        {"params": weights, "weight_decay": cfg_optimizer["weight_decay"]},
+    ]
 
-
-    if cfg_optimizer['type'] == 'sgd':
-        optimizer = optim.SGD(parameters, lr=cfg_optimizer['lr'], momentum=0.9)
-    elif cfg_optimizer['type'] == 'adam':
-        optimizer = optim.Adam(parameters, lr=cfg_optimizer['lr'])
-    elif cfg_optimizer['type'] == 'adamw':
-        optimizer = AdamW(parameters, lr=cfg_optimizer['lr'])
+    if cfg_optimizer["type"] == "sgd":
+        optimizer = optim.SGD(parameters, lr=cfg_optimizer["lr"], momentum=0.9)
+    elif cfg_optimizer["type"] == "adam":
+        optimizer = optim.Adam(parameters, lr=cfg_optimizer["lr"])
+    elif cfg_optimizer["type"] == "adamw":
+        optimizer = AdamW(parameters, lr=cfg_optimizer["lr"])
+    elif cfg_optimizer["type"] == "ranger":
+        optimizer = Ranger21(parameters, lr=cfg_optimizer["lr"], use_madgrad=True)
     else:
-        raise NotImplementedError("%s optimizer is not supported" % cfg_optimizer['type'])
+        raise NotImplementedError(
+            "%s optimizer is not supported" % cfg_optimizer["type"]
+        )
 
     return optimizer
-
 
 
 class AdamW(Optimizer):
@@ -48,8 +54,15 @@ class AdamW(Optimizer):
         https://openreview.net/forum?id=ryQu7f-RZ
     """
 
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
-                 weight_decay=0, amsgrad=False):
+    def __init__(
+        self,
+        params,
+        lr=1e-3,
+        betas=(0.9, 0.999),
+        eps=1e-8,
+        weight_decay=0,
+        amsgrad=False,
+    ):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if not 0.0 <= eps:
@@ -58,14 +71,15 @@ class AdamW(Optimizer):
             raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
         if not 0.0 <= betas[1] < 1.0:
             raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
-        defaults = dict(lr=lr, betas=betas, eps=eps,
-                        weight_decay=weight_decay, amsgrad=amsgrad)
+        defaults = dict(
+            lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, amsgrad=amsgrad
+        )
         super(AdamW, self).__init__(params, defaults)
 
     def __setstate__(self, state):
         super(AdamW, self).__setstate__(state)
         for group in self.param_groups:
-            group.setdefault('amsgrad', False)
+            group.setdefault("amsgrad", False)
 
     def step(self, closure=None):
         """Performs a single optimization step.
@@ -78,33 +92,35 @@ class AdamW(Optimizer):
             loss = closure()
 
         for group in self.param_groups:
-            for p in group['params']:
+            for p in group["params"]:
                 if p.grad is None:
                     continue
                 grad = p.grad.data
                 if grad.is_sparse:
-                    raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
-                amsgrad = group['amsgrad']
+                    raise RuntimeError(
+                        "Adam does not support sparse gradients, please consider SparseAdam instead"
+                    )
+                amsgrad = group["amsgrad"]
 
                 state = self.state[p]
 
                 # State initialization
                 if len(state) == 0:
-                    state['step'] = 0
+                    state["step"] = 0
                     # Exponential moving average of gradient values
-                    state['exp_avg'] = torch.zeros_like(p.data)
+                    state["exp_avg"] = torch.zeros_like(p.data)
                     # Exponential moving average of squared gradient values
-                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+                    state["exp_avg_sq"] = torch.zeros_like(p.data)
                     if amsgrad:
                         # Maintains max of all exp. moving avg. of sq. grad. values
-                        state['max_exp_avg_sq'] = torch.zeros_like(p.data)
+                        state["max_exp_avg_sq"] = torch.zeros_like(p.data)
 
-                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
                 if amsgrad:
-                    max_exp_avg_sq = state['max_exp_avg_sq']
-                beta1, beta2 = group['betas']
+                    max_exp_avg_sq = state["max_exp_avg_sq"]
+                beta1, beta2 = group["betas"]
 
-                state['step'] += 1
+                state["step"] += 1
 
                 # if group['weight_decay'] != 0:
                 #     grad = grad.add(group['weight_decay'], p.data)
@@ -116,15 +132,20 @@ class AdamW(Optimizer):
                     # Maintains the maximum of all 2nd moment running avg. till now
                     torch.max(max_exp_avg_sq, exp_avg_sq, out=max_exp_avg_sq)
                     # Use the max. for normalizing running avg. of gradient
-                    denom = max_exp_avg_sq.sqrt().add_(group['eps'])
+                    denom = max_exp_avg_sq.sqrt().add_(group["eps"])
                 else:
-                    denom = exp_avg_sq.sqrt().add_(group['eps'])
+                    denom = exp_avg_sq.sqrt().add_(group["eps"])
 
-                bias_correction1 = 1 - beta1 ** state['step']
-                bias_correction2 = 1 - beta2 ** state['step']
-                step_size = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
+                bias_correction1 = 1 - beta1 ** state["step"]
+                bias_correction2 = 1 - beta2 ** state["step"]
+                step_size = group["lr"] * math.sqrt(bias_correction2) / bias_correction1
 
                 # p.data.addcdiv_(-step_size, exp_avg, denom)
-                p.data.add_(-step_size,  torch.mul(p.data, group['weight_decay']).addcdiv_(1, exp_avg, denom) )
+                p.data.add_(
+                    -step_size,
+                    torch.mul(p.data, group["weight_decay"]).addcdiv_(
+                        1, exp_avg, denom
+                    ),
+                )
 
         return loss
