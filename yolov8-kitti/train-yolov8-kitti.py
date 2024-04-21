f8dd72d334a64ffd2a3708a4a4de62964f5934eb
Puneet Gupta <puneetgupta.dtu@gmail.com>
2024-04-21 15:24:28 +0530

    Update train-val split
    Signed-off-by: Puneet Gupta <puneetgupta.dtu@gmail.com>

--- yolov8-kitti/train-yolov8-kitti.py
+++ yolov8-kitti/train-yolov8-kitti.py
@@ -13,28 +13,18 @@ from PIL import Image
 os.environ['WANDB_DISABLED'] = "True"
 base_path = '/Users/shuttlesworthneo/Depository/OMSCS/Deep Learning/project/methOD/yolov8-kitti'
 base_data_dir = Path(f'{base_path}/data/KITTI/training')
-img_path = base_data_dir / 'image_2'
-label_path = base_data_dir /'labels_2'
 with open(base_data_dir / 'classes.json','r') as f:
     classes = json.load(f)
-ims = sorted(list(img_path.glob('*')))
-labels = sorted(list(label_path.glob('*')))
-pairs = list(zip(ims,labels))
-train, test = train_test_split(pairs,test_size=0.1,shuffle=True)
 train_path = Path('train').resolve()
 train_path.mkdir(exist_ok=True)
-valid_path = Path('valid').resolve()
+valid_path = Path('val').resolve()
 valid_path.mkdir(exist_ok=True)
-for t_img, t_lb in tqdm(train):
-    im_path = train_path / t_img.name
-    lb_path = train_path / t_lb.name
-    shutil.copy(t_img,im_path)
-    shutil.copy(t_lb,lb_path)
-for t_img, t_lb in tqdm(test):
-    im_path = valid_path / t_img.name
-    lb_path = valid_path / t_lb.name
-    shutil.copy(t_img,im_path)
-    shutil.copy(t_lb,lb_path)
+for x in ['train', 'val']:
+    split_file = os.path.join(f'{base_path}/data/KITTI/', "ImageSets", f"{x}.txt")
+    idx_list = [x.strip() for x in open(split_file).readlines()]
+    for f_name in idx_list:
+        shutil.copy(f'{base_path}/data/KITTI/training/image_2/{f_name}.png',f'{base_path}/{x}/{f_name}.png')
+        shutil.copy(f'{base_path}/data/KITTI/training/label_2/{f_name}.txt', f'{base_path}/{x}/{f_name}.txt')
 yaml_file = 'names:\n'
 yaml_file += '\n'.join(f'- {c}' for c in classes)
 yaml_file += f'\nnc: {len(classes)}'
